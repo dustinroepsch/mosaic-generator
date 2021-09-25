3cc2ee20ac77f6df6c8a9b92ea011ab9302fc41e
Dustin Ryan-Roepsch <dustinroepsch@gmail.com>
2021-09-24 19:30:07 -0700

    can make pretty pictures

---
+++ src/main.rs
@@ -0,0 +1,58 @@
+use image::{GenericImageView, Rgba};
+use std::path::PathBuf;
+use structopt::StructOpt;
+
+#[derive(StructOpt)]
+#[structopt(name = "voronoi", about = "make voronoi")]
+struct Opt {
+    /// The path to the input image
+    input: PathBuf,
+
+    /// The destination of the output image
+    #[structopt(short, default_value = "out.png")]
+    output: PathBuf,
+
+    /// The number of samples to make
+    #[structopt(long, default_value = "100")]
+    num_samples: u32,
+}
+
+struct Node {
+    x: u32,
+    y: u32,
+    color: Rgba<u8>,
+}
+
+impl Node {
+    fn distance_squared(&self, x: u32, y: u32) -> u32 {
+        (self.x - x) * (self.x - x) + (self.y - y) * (self.y - y)
+    }
+}
+
+fn main() {
+    let opt = Opt::from_args();
+    let source_image = image::open(opt.input).unwrap();
+
+    let nodes: Vec<Node> = (0..opt.num_samples)
+        .map(|i| {
+            let n_x = (i * 1312311) % source_image.width();
+            let n_y = (i * 171344566) % source_image.height();
+            Node {
+                x: n_x,
+                y: n_y,
+                color: source_image.get_pixel(n_x, n_y),
+            }
+        })
+        .collect();
+
+    let output_image =
+        image::ImageBuffer::from_fn(source_image.width(), source_image.height(), |x, y| {
+            nodes
+                .iter()
+                .min_by_key(|&n| n.distance_squared(x, y))
+                .unwrap()
+                .color
+        });
+
+    output_image.save(opt.output).unwrap();
+}
